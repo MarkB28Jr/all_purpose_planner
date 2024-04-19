b14742604739ac977c94161e371e2e221d209a0b
Eric Fargo <fargo10k@gmail.com>
2024-04-19 11:06:54 -0400

    Started filling tuple

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 # Create your models here
 
 
-FEATUREDEVENTS = ( #Data in this tuple should look like this ('COM', 'Comic Con') 
+FEATUREDEVENTS = ( #Data in this tuple should look like this ('CcN', 'Comic Con, venue, city / state') 
     ('TcN', 'TwitchCon, San Diego Convention Center, San Diego CA'), # https://www.twitchcon.com/san-diego-2024/  Date (September 20-22 2024)
     ('CcN', 'Comic-Con, San Diego Convention Center, San Diego CA'), # https://www.comic-con.org/cc/  Date (July 25-28 2024)
     ('NYc', 'New York Comic-Con, Javits Center, NYC NY'), # https://www.newyorkcomiccon.com/en-us.html  Date (October 17-20 2024)
