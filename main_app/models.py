e2a9bf48cfcc409a527215717624759a927cf34d
Eric Fargo <fargo10k@gmail.com>
2024-04-19 09:56:55 -0400

    starting models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here
+
+
+
+
+
+
+
+class Tasks(models.Model):
+    name = models.CharField(max_length=50),
+    description = models.TextField(max_length=200),
+    image_url = models.CharField(max_length=150)
+    
+
+    
+
+    
+    
+     
