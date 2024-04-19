a42aeb10c7f2a25ab0b9782f6375439542f9d6bd
Eric Fargo <fargo10k@gmail.com>
2024-04-19 10:51:51 -0400

    Started filling tuple

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here
 
 
 FEATUREDEVENTS = ( #Data in this tuple should look like this ('COM', 'Comic Con') 
-    ('', ''),
-    ('', ''),
-    ('', ''),
+    ('TcN', 'TwitchCon, San Diego Convention Center, San Diego CA'), # https://www.twitchcon.com/san-diego-2024/  Date (September 20-22 2024)
+    ('CcN', 'Comic-Con, San Diego Convention Center, San Diego CA'), # https://www.comic-con.org/cc/  Date (July 25-28 2024)
+    ('NYc', 'New York Comic-Con, Javits Center, NYC NY'), # https://www.newyorkcomiccon.com/en-us.html  Date (October 17-20 2024)
 )
 
 
@@ -15,15 +15,16 @@ FEATUREDEVENTS = ( #Data in this tuple should look like this ('COM', 'Comic Con'
 
 class Tasks(models.Model):
     name = models.CharField(max_length=50),
-    description = models.TextField(max_length=200),
-    image_url = models.CharField(max_length=150)
+    description = models.TextField(max_length=200),# Image needs a default image
+    image_url = models.CharField(max_length=150, default='')
+    
+
     
 class FeaturedEvents(models.Model):
-    description = models.TextField(max_length=200),
-    image_url = models.CharField(max_length=150),
-    location = models.CharField(max_length=100) # When entering this data include  venue, city, and state
+    description = models.TextField(max_length=200),# Image needs a default image
+    image_url = models.CharField(max_length=150, default=''), 
     featuredEvents =models.CharField(
-        max_length=100,
+        max_length=150,
             choices= FEATUREDEVENTS
     )
 
