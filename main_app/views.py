6d763bbdc592dc219d69be587ef429e6ec76242c
MarkB28Jr <mark.brashear265@gmail.com>
2024-04-20 15:43:47 -0500

    Still adding the finishing touchs to creating and views the tasks

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
+#Import
+import uuid
 import boto3
 import os
+# From import
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
@@ -7,10 +10,10 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+
 # Create your views here.
-
-
-
 def home(request):
     return render(request, 'home.html')
 
@@ -19,11 +22,13 @@ def about(request):
 
 @login_required
 def tasks_index(request):
-    return render(request, 'tasks/index.html')
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'tasks/index.html', {'tasks': tasks})
 
 @login_required
-def tasks_detail(request, cat_id):
-    return render(request, 'tasks/detail.html')
+def tasks_detail(request, task_id):
+    task = Task.objects.get(id=task_id)
+    return render(request, 'tasks/detail.html', {'task': task})
 
 def signup(request,):
     error_message = ''
@@ -41,21 +46,24 @@ def signup(request,):
 
 
 
-
 # Class'set
 
 class TaskCreate(LoginRequiredMixin ,CreateView):
-  model = Task
-  fields = ['name', 'description']
+    model = Task
+    fields = ['name', 'description']
   
-  def form_valid(self, form):
-    form.instance.user = self.request.user
-    return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+  
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin ,UpdateView):
-  model = Task
-  fields = ['name', 'description']
+    model = Task
+    fields = ['name', 'description']
 
 class TaskDelete(LoginRequiredMixin ,DeleteView):
-  model = Task
-  success_url = '/tasks'
+    model = Task
+    success_url = '/tasks'
